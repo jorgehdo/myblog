bae613d52939cfc99d855dc2381efbc650a40816
Jorge Espinosa <jorgeespinosa@Jorges-MacBook-Pro-2.local>
2019-09-16 20:52:51 -0500

    layouts

--- app/controllers/estaticas_controller.rb
+++ app/controllers/estaticas_controller.rb
@@ -1,7 +1,12 @@
 class EstaticasController < ApplicationController
+	
   def contacto
+  	@id = params[:id].to_i + 5
+  	@pag = params[:pagina]
   end
 
   def nosotros
+  	@mensajes = "Hola desde el controlador"
+  	@usuarios = ['Pablo', 'Maria', 'Silvia', 'Daniel']
   end
 end
